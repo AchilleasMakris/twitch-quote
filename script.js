79c794593dd98ef0a5bd0f3936b7a26e713c8403
Achilleas Makris <lexlarisa@hotmail.com>
2025-03-01 06:02:38 +0200

    test

--- script.js
+++ script.js
@@ -1,21 +1,8 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('DOM fully loaded and parsed');
-    const client = new tmi.Client({
-        channels: [ 'lexmindset' ]
-    });
-
-    client.connect().then(() => {
-        console.log('Connected to Twitch chat');
-    }).catch(error => {
-        console.error('Error connecting to Twitch chat:', error);
-    });
-
-    client.on('message', (channel, tags, message, self) => {
-        if(self) return;
-
-        console.log(`Received message: ${message}`);
-
-        if(message.toLowerCase() === '!quote') {
+window.addEventListener("onEventReceived", function (e) {
+    let l = e.detail.event.data;
+    if ((console.log(e), "message" === e.detail.listener)) {
+        let message = l.text;
+        if (message.toLowerCase() === '!quote') {
             console.log('Fetching quote...');
             fetch('https://api.quotable.io/random')
                 .then(response => response.json())
@@ -35,5 +22,41 @@ document.addEventListener('DOMContentLoaded', () => {
                 })
                 .catch(error => console.error('Error fetching quote:', error));
         }
-    });
-});
+    }
+
+    if (
+      ("tip" === C || "kofiDonation" === C
+        ? ((goalProgressText.textContent = (
+            userCurrencySymbol + currentGoal
+          ).toString()),
+          (goalText.textContent = (userCurrencySymbol + goal).toString()))
+        : ((goalProgressText.textContent = currentGoal),
+          (goalText.textContent = goal)),
+      "kofiDonation" === C || "kofiSubscriber" === C)
+    )
+
+    if (!e.detail.event)
+    void 0 !== e.detail.event.itemId &&
+      (e.detail.listener = "redemption-latest");
+    let r = e.detail.listener.split("-")[0],
+      n = e.detail.event;
+    "twitch" === provider
+      ? "follower" === r && "follower" === C
+        ? o(C, 1)
+        : "subscriber" === r && "subscriber" === C
+        ? n.bulkGifted
+          ? o(C, n.amount)
+          : n.isCommunityGift || o(C, 1)
+        : (("cheer" === r && "cheer" === C) || ("tip" === r && "tip" === C)) &&
+          o(C, n.amount)
+      : "youtube" === provider &&
+        ("sponsor" === r && "subscriber" === C
+          ? n.bulkGifted
+            ? o(C, n.amount)
+            : n.isCommunityGift || n.bulkGifted || o(C, n.amount)
+          : "subscriber" === r && "subscriber" === C
+          ? o(C, 1)
+          : (("superchat" === r && "superchat" === C) ||
+              ("tip" === r && "tip" === C)) &&
+            o(C, n.amount));
+  });
