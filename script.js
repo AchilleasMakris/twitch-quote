a326ca727dc55cd730fb3f1b3c1247bf5a885bc5
Achilleas Makris <lexlarisa@hotmail.com>
2025-03-01 07:07:53 +0200

    testing

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ client.on("message", (channel, tags, message, self) => {
 
     if (message.toLowerCase() === "!quote") {
         console.log("Fetching quote...");
-        fetch("https://zenquotes.io/api/random")
+        fetch("https://api.adviceslip.com/advice")
             .then(response => response.json())
             .then(data => {
                 console.log("Fetched quote:", data.content);
